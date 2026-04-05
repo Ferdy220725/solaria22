a8006b15eaea462559389e05bcbabe4b917caf02
Ferdy220725 <25025010100@student.upnjatim.ac.id>
2026-04-05 12:40:15 +0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // --- TAMBAHKAN BAGIAN INI (Wajib!) ---
   typescript: {
+    // Ini jurus pamungkas buat paksa build jalan terus walau ada error tipe data
     ignoreBuildErrors: true,
   },
+  // -------------------------------------
   eslint: {
+    // Mengabaikan error linting saat build
     ignoreDuringBuilds: true,
   },
 };
